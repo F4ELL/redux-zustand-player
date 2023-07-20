c899c385ec46c709a5f13148c5093166438c966c
F4ELL <ferreiraperes1234@gmail.com>
2023-07-20 15:28:34 -0300

    feat: redux actions

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,19 @@
 import { useAppSelector } from "../store"
+import { useCurrentLesson } from "../store/slices/player"
 
 export function Header() {
-  const { currentModule, currentLesson } = useAppSelector(state => {
-    const { currentLessonIndex, currentModuleIndex } = state.player
+  const { currentModule, currentLesson } = useCurrentLesson()
+  
+  const isCourseLoading = useAppSelector(state => state.player.isLoading)
 
-    const currentModule = state.player.course.modules[currentModuleIndex]
-    const currentLesson = currentModule.lessons[currentLessonIndex]
-
-    return { currentModule, currentLesson }
-  })
+  if (isCourseLoading) {
+    return <h1 className="text-2xl font-bold">Carregando...</h1>
+  }
 
   return (
     <div className="flex flex-col gap-1">
-      <h1 className="text-2xl font-bold">{currentLesson.title}</h1>
-      <span className="text-sm text-zinc-400">Módulo "{currentModule.title}"</span>
+      <h1 className="text-2xl font-bold">{currentLesson?.title}</h1>
+      <span className="text-sm text-zinc-400">Módulo "{currentModule?.title}"</span>
     </div>
   )
 }
